160b19628b9708d2ce3b07643970f88028b612d8
rschuessler <roderick.schuessler8@googlemail.com>
2018-01-22 13:46:02 +0100

    home zusammen gefügt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { MainImageComponent } from './main-image/main-image.component';
 import { HomeComponent } from './home/home.component';
 import { AngularFireModule } from "angularfire2";
 
@@ -13,8 +11,6 @@ import { AngularFireModule } from "angularfire2";
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    MainImageComponent,
     HomeComponent
   ],
   imports: [
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+
+import $ = require("jquery");
 
 @Component({
   selector: 'app-home',
@@ -10,10 +12,33 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    
+    this.navbarChange() ;
   }
 
-  getImgHeight(event){
-    console.log("home C " + event);
+  navbarChange(){
+    var _height;
+    $(document).ready(function() {
+      //$('nav').css("background-color", "transparent");
+      $( "nav" ).addClass( "navbar-default");
+      $("img").each(function() {
+          console.log("main Image height " + $(this).height());
+           _height =  $(this).height();
+          //console.log($(this).width());
+      });
+      $(window).scroll(function(){
+        if ($(this).scrollTop() >= _height) {
+          //$('nav').css("background-color", "#fff");
+          $( "nav" ).addClass( "navbar-inverse");
+          $( "nav" ).removeClass( "navbar-default");
+          console.log("end of img")
+        } else {
+          //$('nav').css("background-color", "transparent");
+          $( "nav" ).removeClass( "navbar-inverse");
+          $( "nav" ).addClass( "navbar-default");          
+        }
+      });
+      console.log("Parameter: " + _height);
+    });
   }
+  
 }
