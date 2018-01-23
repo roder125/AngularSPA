f51e29ff59b8650d9768274a3975452dfdfee2c2
rschuessler <roderick.schuessler8@googlemail.com>
2018-01-23 11:29:17 +0100

    image display

--- src/app/add-mountain/add-mountain.component.ts
+++ src/app/add-mountain/add-mountain.component.ts
@@ -11,6 +11,15 @@ import { Component, OnInit } from '@angular/core';
 export class AddMountainComponent implements OnInit {
 
   mountain = {} as Mountain;
+  path: string;
+  activeColor: string = 'green';
+  baseColor: string = '#ccc';
+  overlayColor: string = 'rgba(255,255,255,0.5)';
+  
+  dragging: boolean = false;
+  loaded: boolean = false;
+  imageLoaded: boolean = false;
+  imageSrc: string = '';
 
   constructor(private fService: FirebaseDatabaseService) {
 
@@ -28,5 +37,32 @@ export class AddMountainComponent implements OnInit {
     console.log(mountain.name);
     console.log(mountain.height);
   }
+  
+  handleInputChange(e) {
+    var file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
+
+    var pattern = /image-*/;
+    var reader = new FileReader();
+
+    if (!file.type.match(pattern)) {
+        alert('invalid format');
+        return;
+    }
+
+    this.loaded = false;
+
+    reader.onload = this._handleReaderLoaded.bind(this);
+    reader.readAsDataURL(file);
+  }
+
+  _handleReaderLoaded(e) {
+      var reader = e.target;
+      this.imageSrc = reader.result;
+      this.loaded = true;
+  }
+
+  handleImageLoad() {
+    this.imageLoaded = true;
+}
 
 }
